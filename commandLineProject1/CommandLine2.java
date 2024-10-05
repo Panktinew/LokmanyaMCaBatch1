221923a220ed6b8fe7986c76f966c7d775018d7f
Panktinew <130337976+Panktinew@users.noreply.github.com>
2024-10-05 09:05:24 +0530

    Add files via upload

--- commandLineProject1/CommandLine2.java
+++ commandLineProject1/CommandLine2.java
@@ -1,13 +1,39 @@
 package commandLineProject1;
 
+import java.util.List;
+
 public class CommandLine2 {
 
 	public static void main(String[] args) {
 		//javac pr1.java
 		// java pr1 "hi" "hello"  56 78  
 		
-		double s = 0;
+		double s = -13.4;
+		
+		//condition ? True : False  --- short cut of if
+		
+		  String ans= s >0 ? "positive" : "negative";
+		System.out.println(ans);
+
+		
+		
+		 ans= s >0 ?  s%2==0 ? "Even" : "odd"      : "negative";
+		   
+		   
+		   /*if(s >0) {
+			System.out.println("positive");
+		}
+		else {
+			System.out.println("negative");
+			
+		}*/
+		
+		
+		
 		for(int i=0  ; i< args.length ;  i++) {
+			
+			
+			
 			 
 			System.out.println(args[i]);
 			
@@ -17,7 +43,7 @@ public class CommandLine2 {
 		System.out.println(s);
 		
 		
-		
+	//	List<Integer> lst;
 	}
 
 }

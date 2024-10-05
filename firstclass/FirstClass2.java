e1e39bae03e34f439a474d3db16008ee462a46f4
Panktinew <130337976+Panktinew@users.noreply.github.com>
2024-10-05 08:24:01 +0530

    Add files via upload

---
+++ firstclass/FirstClass2.java
@@ -0,0 +1,25 @@
+package firstclass;
+
+public class FirstClass2 {
+
+	public static void main(String[] args) {
+	
+		// object--- variable of class
+		Student st= new Student();
+		
+		st.setrollno(5);
+		
+	
+	 
+	 System.out.println(st.getrollno());
+		
+		int no,no1;
+		no=10;
+		no1=4;
+		
+		System.out.println("result="+(float) no/no1);
+		
+		
+	}
+
+}
---
+++ firstclass/Student.java
@@ -0,0 +1,16 @@
+package firstclass;
+
+public class Student {
+	
+	// variables
+	private int rollno,mobile;
+   private String	name,email; 
+	//functions
+  void setrollno(int no) {
+	    rollno=no;
+  }
+  int getrollno() {
+	  return rollno;
+  }
+
+}
